b531679f33176d7b9b509120f28845e47df9537c
jpaulo233 <joaobranco26@gmail.com>
2026-04-07 20:22:16 -0300

    Add files via upload

---
+++ 25-03exercicio5.js
@@ -0,0 +1,9 @@
+let nome = "Joao"
+let anoDeNascimento = 2000
+let anoAtual = 2026
+let IdadeEm = 2050
+
+console.log("qual o nome dele?", nome);
+console.log("qual a idade dele?", anoAtual - anoDeNascimento); 
+console.log("ela é maior de idade?", ((anoAtual - anoDeNascimento) >= 18));
+console.log("quantos anos ele tera em 2050?", IdadeEm - anoDeNascimento);
---
+++ 25-03operadores-logica-prog.js
@@ -0,0 +1,34 @@
+// Exercicio A 
+// exercicio 1 
+
+let primeiroValor = 3 
+let segundoValor = 4
+let resultado = primeiroValor + segundoValor
+
+console.log(resultado);
+
+// exercicio 2
+
+let primeiroValorA = 3
+let segundoValorB = 5
+let resultadoC = primeiroValorA * segundoValorB 
+
+console.log((resultadoC) / 2);
+
+// exercicio 3
+
+let primeiroValor1 = 4 
+let segundoValor2 = 5
+let resultado3 = primeiroValor1 - segundoValor2 
+
+console.log((resultado3) * -1)
+
+// exercico 4 
+resultadoDaDivisao = 234 % 5 
+
+console.log(resultadoDaDivisao)
+
+//exercicio B 
+//exercicio 1 
+
+
